2ef7d590517c7aad6de8ac4e7d45216d49fd3433
Sarath-Mathew <svinc28@gmail.com>
2022-05-13 06:43:36 +1000

    add join room function add function to send message across rooms

--- server/index.js
+++ server/index.js
@@ -18,6 +18,18 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data)
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("recieve_message", data);
+    
+  });
+  
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
