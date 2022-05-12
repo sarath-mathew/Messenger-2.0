0bc492687747008b3341c6d126c9cefc68b4502e
Sarath-Mathew <svinc28@gmail.com>
2022-05-13 06:40:17 +1000

    add base css elements. add show message option

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import Chat from './Chat';
 import io from "socket.io-client"
-import React, { useEffect, useState } from "react";
+import {React, useState } from "react";
 
 const socket = io.connect("http://localhost:3001")
 
 function App() {
   const [username, setUsername] = useState("")
   const [room, setRoom] = useState("")
+  const [showChat, setShowChat] = useState(false)
 
   const joinRoom = () => {
     if (username !== '' && room != "") {
       socket.emit("join_room", room)
+      setShowChat(true);
       }
     };
 
   return (
     <div className="App">
+      {!showChat ? (
+
+      <div className="joinChatContainer">
       <h3>Join A Chat</h3>
       <input 
       type="text" placeholder="i.e Sarath..." 
@@ -31,8 +36,11 @@ function App() {
         }}
       />
       <button onClick={joinRoom}>Join a Room</button>
-
+        </div>
+        )
+      : (
       <Chat socket={socket} username={username} room={room}/>
+        )}
     </div>
   );
 }
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function Chat({socket, username, room}) {
+function Chat({ socket, username, room }) {
+    const [currentMessage, setCurrentMessage] = useState("")
+    const [messageList, setMessageList] = useState([])
+
+    const sendMessage = async () => {
+        if (currentMessage != "") {
+            const messageData = {
+                room: room,
+                author: username,
+                message: currentMessage,
+                time: 
+                new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(), 
+            };
+
+            await socket.emit("send_message", messageData);
+        }
+    }
+    
+    useEffect(() => {
+        socket.on("recieve_message", (data) => {
+            setMessageList((list) => [...list, data]) // takes the current list of message and appends the new message.
+            console.log(data);
+    });
+  }, [socket]);
+
     return (
-        <div>
-            <div className='chat-header'> 
-            <p>Live Chat</p>
-            </div>            
-            <div className='chat-body'> </div>            
-            <div className='chat-footer'> 
-                <input type="text" placeholder="hey" />
-                <button>&#9658;</button>
-            
-            </div>            
+        <div class="chat-window">
+            <div className='chat-header'>
+                <p>Live Chat</p>
+            </div>
+            <div className='chat-body'> </div>
+            <div className='chat-footer'>
+                <input
+                    type="text"
+                    placeholder="hey"
+                    onChange={(event) => {
+                        setCurrentMessage(event.target.value);
+                    }}
+                />
+                <button onClick={sendMessage}>&#9658;</button>
+
+            </div>
 
         </div>
     )
