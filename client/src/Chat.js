13b6a4621b4d5e85cfebda2d37f46d11a25be34b
Sarath-Mathew <svinc28@gmail.com>
2022-05-15 19:18:59 +1000

    fix duplication bug add css styling to messages

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -10,27 +10,45 @@ function Chat({ socket, username, room }) {
                 room: room,
                 author: username,
                 message: currentMessage,
-                time: 
-                new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(), 
+                time:
+                    new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
             };
-
+            console.log(messageData)
             await socket.emit("send_message", messageData);
+            setMessageList((list) => [...list, messageData])
         }
     }
-    
+
     useEffect(() => {
-        socket.on("recieve_message", (data) => {
-            setMessageList((list) => [...list, data]) // takes the current list of message and appends the new message.
-            console.log(data);
-    });
-  }, [socket]);
+        socket.on("receive_message", (info) => {
+            console.log(info)
+            setMessageList((list) => [...list, info])
+
+        });
+    }, [socket]);
 
     return (
         <div class="chat-window">
             <div className='chat-header'>
                 <p>Live Chat</p>
             </div>
-            <div className='chat-body'> </div>
+            <div className='chat-body'>
+                {messageList.map((messageContent) => {
+                    return <div className="message">
+                        <div>
+                            <div className="message-content">
+                                <p>{messageContent.message}</p>
+                            </div>
+
+                            <div className="message-meta">
+                                <p>{messageContent.time}</p>
+                                <p>{messageContent.author}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                })}
+            </div>
             <div className='chat-footer'>
                 <input
                     type="text"
